7813041267cecb8c09accd5f6cffa393259e2b8c
Afaandii <fandisantuy140@gmail.com>
2026-07-21 22:54:57 +0700

    content: rewrite and check data type all entities master modul

--- backend/internal/modules/master/outlets/entity.go
+++ backend/internal/modules/master/outlets/entity.go
@@ -8,13 +8,13 @@ import (
 
 type Outlets struct {
 	IDOutlet     uint   `json:"id_outlet" gorm:"primaryKey;autoIncrement;column:id_outlet"`
-	OutletCode   string `json:"outlet_code" gorm:"type:varchar(255);column:outlet_code"`
-	OutletName   string `json:"outlet_name" gorm:"type:varchar(255);column:outlet_name"`
-	Address      string `json:"address" gorm:"type:varchar(255);column:address"`
-	City         string `json:"city" gorm:"type:varchar(255);column:city"`
-	OpeningHours datatypes.Time `json:"opening_hours" gorm:"column:opening_hours"`
-	ClosingHours datatypes.Time `json:"closing_hours" gorm:"column:closing_hours"`
-	PhoneNumber *int `json:"phone_number" gorm:"type:int;column:phone_number"`
+	OutletCode   string `json:"outlet_code" gorm:"type:varchar(125);column:outlet_code"`
+	OutletName   string `json:"outlet_name" gorm:"type:varchar(125);column:outlet_name"`
+	Address      string `json:"address" gorm:"type:text;column:address"`
+	City         string `json:"city" gorm:"type:varchar(60);column:city"`
+	OpeningHours datatypes.Time `json:"opening_hours" gorm:"type:time;column:opening_hours"`
+	ClosingHours datatypes.Time `json:"closing_hours" gorm:"type:time;column:closing_hours"`
+	PhoneNumber *string `json:"phone_number" gorm:"type:varchar(20);column:phone_number"`
 	StatusOutlet string `json:"status_outlet" gorm:"type:varchar(255);column:status_outlet"`
 	CreatedAt 	 time.Time `json:"created_at"`
 	UpdatedAt 	 time.Time `json:"updated_at"`
