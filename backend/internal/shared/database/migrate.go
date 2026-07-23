427f7a3af792b8e9e69a6208b9c5c233566efd26
Afaandii <fandisantuy140@gmail.com>
2026-07-23 20:39:07 +0700

    content: initial migrate entities

--- backend/internal/shared/database/migrate.go
+++ backend/internal/shared/database/migrate.go
@@ -1,7 +1,14 @@
 package database
 
 import (
+	"backend/internal/modules/master/categories"
+	"backend/internal/modules/master/ingredients"
+	"backend/internal/modules/master/outlets"
 	"backend/internal/modules/master/roles"
+	"backend/internal/modules/master/suppliers"
+	"backend/internal/modules/master/units"
+	"backend/internal/modules/master/users"
+	wirehouse "backend/internal/modules/master/wirehouses"
 
 	"gorm.io/gorm"
 )
@@ -9,5 +16,12 @@ import (
 func AutoMigrate(db *gorm.DB) {
 	db.AutoMigrate(
 		&roles.Roles{},
+		&categories.Category{},
+		&users.Users{},
+		&wirehouse.Wirehouse{},
+		&units.Units{},
+		&outlets.Outlets{},
+		&suppliers.Suppliers{},
+		&ingredients.Ingredients{},
 	)
 }
