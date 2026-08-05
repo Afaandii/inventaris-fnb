8c6c2229bb7058548372484e2a9300555143a84f
Afaandii <fandisantuy140@gmail.com>
2026-08-05 19:50:02 +0700

    content: create repo,service,handler,route for management crud units

---
+++ backend/internal/modules/master/units/handler.go
@@ -0,0 +1,122 @@
+package units
+
+import (
+	"backend/internal/shared/response"
+	"backend/internal/shared/validator"
+	"net/http"
+	"strconv"
+
+	"github.com/gin-gonic/gin"
+)
+
+type Handler struct {
+	service ServiceUnit
+}
+
+func NewHandlerUnit(service ServiceUnit) *Handler {
+	return &Handler{service}
+}
+
+func (h *Handler) GetAll(ctx *gin.Context) {
+	unt, err := h.service.GetAll()
+	if err != nil {
+		response.Error(ctx, http.StatusBadRequest, "Failed to retrieved data unit!", err.Error())
+		return
+	}
+
+	response.Success(ctx, http.StatusOK, "Received data unit successfully!", unt)
+}
+
+func (h *Handler) GetById(ctx *gin.Context) {
+	id_unit, err := strconv.Atoi(ctx.Param("id_unit"))
+	if err != nil {
+		response.Error(ctx, http.StatusBadRequest, "Invalid id unit!", err.Error())
+		return
+	}
+
+	unt, err := h.service.GetById(uint(id_unit))
+	if err != nil {
+		response.Error(ctx, http.StatusInternalServerError, "Failed to retrieved data unit by id!", err.Error())
+		return
+	}
+
+	response.Success(ctx, http.StatusOK, "Received data unit by id successfully!", unt)
+}
+
+func (h *Handler) Create(ctx *gin.Context) {
+	var req struct {
+		UnitCode  string `json:"unit_code" validate:"required,min=3,max=255"`
+		UnitName  string `json:"unit_name" validate:"required,min=3,max=150"`
+		Type      string `json:"type" validate:"required,min=3,max=180"`
+		ShortName string `json:"short_name" validate:"required,min=3,max=80"`
+		IsActive  bool   `json:"is_active" validate:"required,boolean"`
+	}
+
+	if err := ctx.ShouldBindJSON(&req); err != nil {
+		response.Error(ctx, http.StatusBadRequest, "Invalid request body!", err.Error())
+		return
+	}
+
+	if errMap := validator.Validate(req); errMap != nil {
+		response.Error(ctx, http.StatusUnprocessableEntity, "Validator fail!", errMap)
+		return
+	}
+
+	unt, err := h.service.Create(req.UnitCode, req.UnitName, req.Type, req.ShortName, req.IsActive)
+	if err != nil {
+		response.Error(ctx, http.StatusInternalServerError, "Failed create data unit!", err.Error())
+		return
+	}
+
+	response.Success(ctx, http.StatusCreated, "Created data unit successfully!", unt)
+}
+
+func (h *Handler) Update(ctx *gin.Context) {
+	id_unit, err := strconv.Atoi(ctx.Param("id_unit"))
+	if err != nil {
+		response.Error(ctx, http.StatusBadRequest, "Invalid id unit!", err.Error())
+		return
+	}
+
+	var req struct {
+		UnitCode  string `json:"unit_code" validate:"required,min=3,max=255"`
+		UnitName  string `json:"unit_name" validate:"required,min=3,max=150"`
+		Type      string `json:"type" validate:"required,min=3,max=180"`
+		ShortName string `json:"short_name" validate:"required,min=3,max=80"`
+		IsActive  bool   `json:"is_active" validate:"required,boolean"`
+	}
+
+	if err := ctx.ShouldBindJSON(&req); err != nil {
+		response.Error(ctx, http.StatusBadRequest, "Invalid request body!", err.Error())
+		return
+	}
+
+	if errMap := validator.Validate(req); errMap != nil {
+		response.Error(ctx, http.StatusUnprocessableEntity, "Validator fail!", errMap)
+		return
+	}
+
+	unt, err := h.service.Update(uint(id_unit), req.UnitCode, req.UnitName, req.Type, req.ShortName, req.IsActive)
+	if err != nil {
+		response.Error(ctx, http.StatusInternalServerError, "Failed update data unit!", err.Error())
+		return
+	}
+
+	response.Success(ctx, http.StatusOK, "Updated data unit successfully!", unt)
+}
+
+func (h *Handler) Delete(ctx *gin.Context) {
+	id_unit, err := strconv.Atoi(ctx.Param("id_unit"))
+	if err != nil {
+		response.Error(ctx, http.StatusBadRequest, "Invalid id unit!", err.Error())
+		return
+	}
+
+	err = h.service.Delete(uint(id_unit))
+	if err != nil {
+		response.Error(ctx, http.StatusInternalServerError, "Failed deteled data unit!", err.Error())
+		return
+	}
+
+	response.Success(ctx, http.StatusOK, "Deleted data unit successfully!", nil)
+}
---
+++ backend/internal/modules/master/units/repository.go
@@ -0,0 +1,52 @@
+package units
+
+import (
+	"backend/internal/shared/model"
+
+	"gorm.io/gorm"
+)
+
+type UnitRepository interface {
+	GetAll() ([]model.Units, error)
+	GetById(id_unit uint) (*model.Units, error)
+	Create(units *model.Units) error
+	Update(units *model.Units) error
+	Delete(id_unit uint) error
+}
+
+type unitRepository struct {
+	db *gorm.DB
+}
+
+func NewUnitRepository(db *gorm.DB) UnitRepository {
+	return &unitRepository{db}
+}
+
+func (req *unitRepository) GetAll() ([]model.Units, error) {
+	var data []model.Units
+	err := req.db.Find(&data).Error
+
+	return data, err
+}
+
+func (req *unitRepository) GetById(id_unit uint) (*model.Units, error) {
+	var data model.Units
+	err := req.db.First(&data, "id_unit=?", id_unit).Error
+	if err != nil {
+		return nil, err
+	}
+
+	return &data, err
+}
+
+func (req *unitRepository) Create(unit *model.Units) error {
+	return req.db.Create(unit).Error
+}
+
+func (req *unitRepository) Update(unit *model.Units) error {
+	return req.db.Save(unit).Error
+}
+
+func (req *unitRepository) Delete(id_unit uint) error {
+	return req.db.Delete(&model.Units{}, "id_unit=?", id_unit).Error
+}
